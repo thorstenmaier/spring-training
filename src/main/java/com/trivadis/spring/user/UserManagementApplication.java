11ac410ceea065553e79bded732f000c9f105e38
Thorsten Maier <Thorsten.Maier@trivadis.com>
2021-05-21 15:33:28 +0200

    Security

--- src/main/java/com/trivadis/spring/user/UserManagementApplication.java
+++ src/main/java/com/trivadis/spring/user/UserManagementApplication.java
@@ -1,23 +1,12 @@
 package com.trivadis.spring.user;
 
-import com.trivadis.spring.user.service.UserService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class UserManagementApplication implements CommandLineRunner {
+public class UserManagementApplication {
 
-	@Autowired
-	private UserService userService;
-
-	public static void main(String[] args) {
-		SpringApplication.run(UserManagementApplication.class, args);
-	}
-
-	@Override
-	public void run(String... args) throws Exception {
-		userService.getAllUsers().forEach(System.out::println);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(UserManagementApplication.class, args);
+    }
 }
--- src/main/java/com/trivadis/spring/user/service/UserServiceImpl.java
+++ src/main/java/com/trivadis/spring/user/service/UserServiceImpl.java
@@ -2,6 +2,8 @@ package com.trivadis.spring.user.service;
 
 import java.util.List;
 
+import javax.annotation.security.RolesAllowed;
+
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -23,15 +25,18 @@ public class UserServiceImpl implements UserService {
 	}
 
 	@Monitor
+	@RolesAllowed({"USER", "ADMIN"})
 	public List<User> getAllUsers() {
 		return userRepository.findAll();
 	}
 
+	@RolesAllowed("USER")
 	public User findUserById(Long id) {
 		return userRepository.findById(id).orElse(null);
 	}
 
 	@Transactional
+	@RolesAllowed("ADMIN")
 	public User createUser(User user) {
 		logEntryRepository.save(new LogEntry("User created"));
 		return userRepository.save(user);
