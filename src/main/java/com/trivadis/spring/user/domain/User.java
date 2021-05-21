a3eae93b67aa43c649e2c2524da71caa9e9a45b0
Thorsten Maier <Thorsten.Maier@trivadis.com>
2021-05-21 15:33:22 +0200

    DB

--- src/main/java/com/trivadis/spring/user/domain/User.java
+++ src/main/java/com/trivadis/spring/user/domain/User.java
@@ -1,7 +1,17 @@
 package com.trivadis.spring.user.domain;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "Users")
 public class User {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Long id;
 	private String firstname;
 	private String lastname;
