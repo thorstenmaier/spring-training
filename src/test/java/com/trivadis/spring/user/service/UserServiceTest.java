b4ad519c86493b15aa8d358c7eaee713db6f6caa
Thorsten Maier <Thorsten.Maier@trivadis.com>
2021-05-21 17:23:30 +0200

    Test

---
+++ src/test/java/com/trivadis/spring/user/service/UserServiceTest.java
@@ -0,0 +1,26 @@
+package com.trivadis.spring.user.service;
+
+import com.trivadis.spring.user.domain.User;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@ExtendWith(SpringExtension.class) // kann entfallen, da in @SpringBootTest enthalten
+@SpringBootTest
+public class UserServiceTest {
+
+    @Autowired
+    private UserService userService;
+
+    @Test
+    public void testGetAllUsers() {
+        List<User> users = userService.getAllUsers();
+        assertEquals(3, users.size());
+    }
+}
