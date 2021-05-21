375d83b65ec406ded74312850b1f07feac71cc74
Thorsten Maier <Thorsten.Maier@trivadis.com>
2021-05-21 16:36:47 +0200

    @Bean

---
+++ src/main/java/com/trivadis/spring/user/UserManagementConfiguration.java
@@ -0,0 +1,14 @@
+package com.trivadis.spring.user;
+
+import com.trivadis.spring.user.service.UserService;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+//@Configuration
+public class UserManagementConfiguration {
+
+//	@Bean
+//	public UserService userService() {
+//		return new UserService();
+//	}
+}
