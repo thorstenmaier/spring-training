9c0342816b5344f0ec0d7aaefab93557f85184dd
Thorsten Maier <Thorsten.Maier@trivadis.com>
2021-05-21 15:33:28 +0200

    Security

--- src/main/java/com/trivadis/spring/user/ServiceClient.java
+++ src/main/java/com/trivadis/spring/user/ServiceClient.java
@@ -3,7 +3,7 @@ package com.trivadis.spring.user;
 import com.trivadis.spring.user.service.UserService;
 import org.springframework.stereotype.Component;
 
-@Component
+//@Component
 public class ServiceClient {
 
 	public ServiceClient(UserService userService) {
--- src/main/java/com/trivadis/spring/user/service/UserServiceImpl.java
+++ src/main/java/com/trivadis/spring/user/service/UserServiceImpl.java
@@ -2,6 +2,7 @@ package com.trivadis.spring.user.service;
 
 import java.util.List;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -23,21 +24,25 @@ public class UserServiceImpl implements UserService {
 	}
 
 	@Monitor
+	@PreAuthorize("hasRole('ADMIN')")
 	public List<User> getAllUsers() {
 		return userRepository.findAll();
 	}
 
+	@PreAuthorize("isAuthenticated()")
 	public User findUserById(Long id) {
 		return userRepository.findById(id).orElse(null);
 	}
 
 	@Transactional
+	@PreAuthorize("hasRole('ADMIN')")
 	public User createUser(User user) {
 		logEntryRepository.save(new LogEntry("User created"));
 		return userRepository.save(user);
 	}
 
 	@Override
+	@PreAuthorize("hasRole('ADMIN')")
 	public void deleteUserById(Long id) {
 		userRepository.deleteById(id);
 	}
