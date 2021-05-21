ff8a73053222a2d045c40b025d99a8352a2b36fd
Thorsten Maier <Thorsten.Maier@trivadis.com>
2021-05-21 16:55:14 +0200

    CommandLineRunner

--- src/main/java/com/trivadis/spring/user/UserManagementApplication.java
+++ src/main/java/com/trivadis/spring/user/UserManagementApplication.java
@@ -1,12 +1,23 @@
 package com.trivadis.spring.user;
 
+import com.trivadis.spring.user.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class UserManagementApplication {
+public class UserManagementApplication implements CommandLineRunner {
+
+	@Autowired
+	private UserService userService;
 
 	public static void main(String[] args) {
 		SpringApplication.run(UserManagementApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		userService.getAllUsers().forEach(System.out::println);
 	}
 }
