6b9f9645bc7501d372fda2c8a11ba58384f89b29
Thorsten Maier <Thorsten.Maier@trivadis.com>
2021-05-21 17:05:15 +0200

    Properties

--- src/main/java/com/trivadis/spring/user/service/UserServiceImpl.java
+++ src/main/java/com/trivadis/spring/user/service/UserServiceImpl.java
@@ -1,16 +1,26 @@
 package com.trivadis.spring.user.service;
 
 import com.trivadis.spring.user.domain.User;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.List;
 
 @Component
 public class UserServiceImpl implements UserService {
 
+	@Value("${user.firstnames}")
+	private String[] firstnames;
+
+	@Value("${user.lastnames}")
+	private String[] lastnames;
+
 	public List<User> getAllUsers() {
-		return Arrays.asList(new User("Guenther", "Nubert"), new User("Bud", "Spencer"),
-				new User("Dieter", "Develop"));
+		List<User> users = new ArrayList<>();
+		for (int i = 0; i < firstnames.length; i++) {
+			users.add(new User(firstnames[i], lastnames[i]));
+		}
+		return users;
 	}
 }
