56e49ade5a3d874c757b6df0b6ea444490c1e3a4
Thorsten Maier <Thorsten.Maier@trivadis.com>
2022-06-27 16:43:56 +0200

    Use @MockBean instead of static inner @TestConfiguration class

--- src/test/java/com/trivadis/spring/user/service/UserServiceTest.java
+++ src/test/java/com/trivadis/spring/user/service/UserServiceTest.java
@@ -3,12 +3,9 @@ package com.trivadis.spring.user.service;
 import com.trivadis.spring.user.domain.User;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.context.TestConfiguration;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Primary;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.util.List;
@@ -22,18 +19,12 @@ public class UserServiceTest {
     @Autowired
     private UserService userService;
 
+    @MockBean
+    private LogService logService;
+
     @Test
     public void testGetAllUsers() {
         List<User> users = userService.getAllUsers();
         assertEquals(3, users.size());
-    }
-
-    @TestConfiguration
-    public static class TestConfig {
-        @Bean
-        @Primary
-        public LogService mockLogService() {
-            return Mockito.mock(LogService.class);
-        }
     }
 }
