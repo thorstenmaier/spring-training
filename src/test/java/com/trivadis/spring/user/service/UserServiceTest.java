cd6513030fff6bdf84ae71baccfca5cf50271144
Thorsten Maier <Thorsten.Maier@trivadis.com>
2022-06-27 16:42:34 +0200

    Use static inner @TestConfiguration class

--- src/test/java/com/trivadis/spring/user/service/UserServiceTest.java
+++ src/test/java/com/trivadis/spring/user/service/UserServiceTest.java
@@ -5,6 +5,9 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Primary;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.util.List;
@@ -12,7 +15,7 @@ import java.util.List;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @ExtendWith(SpringExtension.class) // kann entfallen, da in @SpringBootTest enthalten
-@SpringBootTest(classes = TestConfig.class)
+@SpringBootTest
 public class UserServiceTest {
 
     @Autowired
@@ -22,5 +25,14 @@ public class UserServiceTest {
     public void testGetAllUsers() {
         List<User> users = userService.getAllUsers();
         assertEquals(3, users.size());
+    }
+
+    @TestConfiguration
+    public static class TestConfig {
+        @Bean
+        @Primary
+        public LogService dummyLogService() {
+            return s -> {};
+        }
     }
 }
